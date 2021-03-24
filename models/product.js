ae3c51acaa681140465493c18d9f185348d11a0d
James Norris <jnorris@gmail.com>
2021-03-24 12:50:57 -0700

    initial commit

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const debug = require('debug')('mongoose-store:models');
+debug('foo');
+
+const productSchema = new Schema({
+   name: String,
+   description: String,
+   img: String,
+   price: Number,
+   qty: Number 
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
+
+const product1 = {
+   name: "Niko",
+   description: "A cat",
+   img: "",
+   price: 500,
+   qty: 1
+};
+
+Product.create(product1).then((p) => { debug(p); }, (error) => { debug(error); });
+
+module.exports = Product;
